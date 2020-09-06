f001a28057159ef67ffdab58ea38c3f07c4cf325
Shadab166 <70801856+Shadab166@users.noreply.github.com>
2020-09-06 08:48:06 +0530

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n;j++)
+        {
+            if(j<=i)
+            {
+                cout<<" * ";
+            }
+            else
+            {
+                cout<<" ";
+            }
+        }
+        cout<<"\n";
+    }
+   
+    return 0;
+}
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n;j++)
+        {
+            if(j<=i)
+            {
+                cout<<" * ";
+            }
+            else
+            {
+                cout<<" ";
+            }
+        }
+        cout<<"\n";
+    }
+   
+    return 0;
+}
+
+Output-
+*
+**
+***
+****
+*****
+
+
+
+
