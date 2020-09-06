559345bd7e7450ed7c609362b62c36ec40340c5e
Shadab166 <70801856+Shadab166@users.noreply.github.com>
2020-09-06 08:50:23 +0530

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -21,37 +21,11 @@ int main()
         }
         cout<<"\n";
     }
-   
     return 0;
 }
 
 
-#include <iostream>
 
-using namespace std;
-
-int main()
-{
-    int n;
-    cin>>n;
-    for(int i=1;i<=n;i++)
-    {
-        for(int j=1;j<=n;j++)
-        {
-            if(j<=i)
-            {
-                cout<<" * ";
-            }
-            else
-            {
-                cout<<" ";
-            }
-        }
-        cout<<"\n";
-    }
-   
-    return 0;
-}
 
 Output-
 *
